156d85254d64212c64c77a6a01107e6052ac5fed
RUBEN CERPA JIMENEZ <ruben.cerpa@ibm.com>
2024-03-19 18:13:34 -0600

    merge intervals 2

--- src/JavaConcept/MergeIntervals.java
+++ src/JavaConcept/MergeIntervals.java
@@ -3,7 +3,6 @@ package JavaConcept;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -48,6 +47,30 @@ public class MergeIntervals {
         return list;
     }
 
+    public static int[][] merge2(int[][] intervals){
+        if(intervals.length == 0) return new int[][]{};
+        if(intervals.length == 1) return intervals;
+
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        List<int[]> response = new ArrayList<>();
+        for (int i = 1; i < intervals.length; i++) {
+            int nextStart = intervals[i][0];
+            int nextEnd = intervals[i][1];
+            if(nextStart <= end){
+                end = Math.max(nextEnd,end);
+            }else{
+                response.add(new int[]{start,end});
+                start = nextStart;
+                end = nextEnd;
+            }
+        }
+        response.add(new int[]{start,end});
+
+        return response.toArray(new int[0][0]);
+    }
+
 
 
 
@@ -56,6 +79,32 @@ public class MergeIntervals {
 class MergeIntervalsTest{
     @Test
     public void mergeTest(){
+        int[][] inter = new int[][]{
+                {1,3},
+                {2,6},
+                {8,10},
+                {15,18},
+        };
+        int[][] expect = new int[][]{
+                {1, 6}, {8, 10}, {15, 18}
+        };
+        assertEquals(expect.length,MergeIntervals.merge2(inter).length);
+    }
+    @Test
+    public void mergeTest1(){
+        int[][] inter = new int[][]{
+                {1,3},
+                {2,6},
+                {8,10},
+                {15,18},
+        };
+        int[][] expect = new int[][]{
+                {1, 6}, {8, 10}, {15, 18}
+        };
+        assertEquals(expect,MergeIntervals.merge2(inter));
+    }
+    @Test
+    public void mergeTest2(){
         int[][] inter = new int[][]{
                 {1,3},
                 {2,6},
