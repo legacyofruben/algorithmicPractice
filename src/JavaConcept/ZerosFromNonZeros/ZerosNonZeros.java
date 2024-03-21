0ad33f48ec95be36fdb149d93ac587dc31ab7be3
RUBEN CERPA JIMENEZ <ruben.cerpa@ibm.com>
2024-03-20 18:41:51 -0600

    ZerosMovesFrontAndEnd

--- src/JavaConcept/ZerosFromNonZeros/ZerosNonZeros.java
+++ src/JavaConcept/ZerosFromNonZeros/ZerosNonZeros.java
@@ -41,6 +41,29 @@ public class ZerosNonZeros {
         }
         return Arrays.toString(inputArray);
     }
+
+    public static String moveZerosToFront(int[] inputArray){
+        //base case
+        if(inputArray==null) return null;
+        if(inputArray.length == 0) return Arrays.toString(new int[]{});
+        if(inputArray.length == 1) return Arrays.toString(inputArray);
+
+        int n = inputArray.length-1;
+        int index = n;
+        while(index>=0){
+            if(inputArray[index] !=0 ){
+                inputArray[n--] = inputArray[index--];
+            }else if(inputArray[index]==0){
+                index--;
+            }
+        }
+        while(n >= 0){
+            inputArray[n--] = 0;
+        }
+        return Arrays.toString(inputArray);
+    }
+
+
 }
 class ZerosNonZerosTest{
     @Test
@@ -63,6 +86,31 @@ class ZerosNonZerosTest{
     public void moveZerosToEndTest_4(){
         assertEquals(Arrays.toString(new int[]{-4, 1, 2, 21, 4, 0, 0}),
                 ZerosNonZeros.moveZerosToEnd(new int[]{-4, 1, 0, 0, 2, 21, 4}));
+    }
+
+    /**
+     *  Move Zeros to front
+     */
+    @Test
+    public void moveZerosToFrontTest_1(){
+        assertEquals(Arrays.toString(new int[]{0, 0, 0, 12, 7, 8, 3}),
+                ZerosNonZeros.moveZerosToFront(new int[]{12, 0, 7, 0, 8, 0, 3}));
+    }
+    @Test
+    public void moveZerosToFrontTest_2(){
+        assertEquals(Arrays.toString(new int[]{0, 0, 0, 1, -5, 8, 1}),
+                ZerosNonZeros.moveZerosToFront(new int[]{1, -5, 0, 0, 8, 0, 1}));
+    }
+
+    @Test
+    public void moveZerosToFrontTest_3(){
+        assertEquals(Arrays.toString(new int[]{0, 0, 0, 1, 1, -5, 4}),
+                ZerosNonZeros.moveZerosToFront(new int[]{0, 1, 0, 1, -5, 0, 4}));
+    }
+    @Test
+    public void moveZerosToFrontTest_4(){
+        assertEquals(Arrays.toString(new int[]{0, 0, -4, 1, 2, 21, 4}),
+                ZerosNonZeros.moveZerosToFront(new int[]{-4, 1, 0, 0, 2, 21, 4}));
     }
 
 }
