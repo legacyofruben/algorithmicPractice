36c5236b013429763e8e206ddc9580f21538f7b6
legacyofruben <legacy.of.ruben@gmail.com>
2024-08-28 20:52:54 -0600

    Delete out folder

--- src/ProjectEuler/Multiples/Multiples.java
+++ src/ProjectEuler/Multiples/Multiples.java
@@ -6,24 +6,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Multiples {
 
-    public static int solution(int limit){
+    public static int solution(int limit) {
         int count = 0;
-        while(limit-- > 0){
-            if((limit % 3) == 0 || (limit % 5) == 0 ){
-                count+=limit;
+        while (limit-- > 0) {
+            if ((limit % 3) == 0 || (limit % 5) == 0) {
+                count += limit;
                 //System.out.println(limit);
             }
         }
         return count;
     }
 }
- class MultiplesTest{
+
+class MultiplesTest {
     @Test
-     public void test_01(){
-        assertEquals(23,Multiples.solution(10));
+    public void test_01() {
+        assertEquals(23, Multiples.solution(10));
     }
-     @Test
-     public void test_02(){
-         assertEquals(23,Multiples.solution(1000));
-     }
- }
+
+    @Test
+    public void test_02() {
+        assertEquals(233168, Multiples.solution(1000));
+    }
+}
